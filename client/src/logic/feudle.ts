8cb64d92fe6985e4617ee4c542928260e80676b0
Ishaan Mathur <imathur3@illinois.edu>
2022-05-20 21:14:38 -0500

    Added game logic

---
+++ client/src/logic/feudle.ts
@@ -0,0 +1,65 @@
+interface Letter {
+    guessed: boolean
+    in_word: boolean
+    in_position: boolean
+}
+
+class Feudle {
+    private _word: string
+    private _total_gusses: number
+    private _guesses: number
+    private _guess: string
+    private _letter_map: Map<string, Letter>
+
+    constructor() {
+        this._word = "";
+        this._guess = ""
+        this._total_gusses = 6;
+        this._guesses = 0;
+
+        let alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        this._letter_map = new Map<string, Letter>();
+        for (let i = 0; i < alphabet.length; i++) {
+            this._letter_map.set(alphabet[i], {
+                guessed: false,
+                in_word: false,
+                in_position: false
+            });
+        }
+    }
+
+    set_word(word: string) {
+        this._word = word.toUpperCase();
+    }
+
+    check_win(): boolean {
+        return this._guess == this._word;
+    }
+
+    check_lose(): boolean {
+        return this._guesses >= this._total_gusses;
+    }
+
+    guess(guess: string): boolean {
+        this._guess = guess.toUpperCase();
+        for (let i = 0; i < this._guess.length; i++) {
+            let char = this._guess.charAt(i);
+            if (this._letter_map.has(char)) {
+                this._letter_map.get(char).guessed = true;
+                if (this._word.includes(char)) {
+                    this._letter_map.get(char).in_word = true;
+                }
+            }
+        }
+
+        for (let i = 0; i < this._guess.length; i++) {
+            let char = this._guess.charAt(i);
+            if (char == this._word.charAt(i)) {
+                this._letter_map.get(char).in_position = true;
+            }
+        }
+
+        this._guesses++;
+        return this.check_win();
+    }
+}
